3279cc3bf720eacd6bf1598d4c286a53cf3a0f79
John Pickett <john-pickett@users.noreply.github.com>
2022-01-08 20:46:00 -0600

    Player joining game correctly

--- ts/app.ts
+++ ts/app.ts
@@ -42,19 +42,18 @@ httpServer.listen(port, () => {
 	console.log(`Server running at port: ${port}`);
 });
 
-const emitEvent = async (event: string, socketID: string, payload: {}) => {
-  console.log('emitting event ', payload);
-  io.to(socketID).emit(event, payload);
-}
+// const emitEvent = async (event: string, socketID: string, payload: {}) => {
+//   console.log('emitting event ', payload);
+//   io.to(socketID).emit(event, payload);
+// }
 
-// TODO: What if we use the Game ShortCode as the socket_id for all players in that game?
 io.on('connection', async (socket: any) => {
   console.log(`user id ${socket.id} connected`);
   const socket_id = socket.id;
 
   socket.join(socket_id); // joins to unique user socket
   io.to(socket_id).emit("connected", {
-    action: "set_socket",
+    action: "set_socket", // unique player socket
     id: socket.id
   });
 
@@ -62,10 +61,6 @@ io.on('connection', async (socket: any) => {
   
 });
 
-io.on('onAny', (socket: any) => {
-  console.log('on any ', socket);
-  
-});
 
 
 
@@ -88,13 +83,14 @@ app.post('/populate', async (req: any, res: any) => {
 app.post('/new-game', async (req: any, res: any) => {
   const { player } = req.body;
   // console.log(req.body);
+
   
   try {
+    const socket = req.app.get("socket");
     const playerRec = await createNewPlayerRecord(player);
     const game = await openNewGame(playerRec);
     const short_code = game.short_code;
-    // TODO: Different channel for connection vs game events?
-    emitEvent("connected", short_code, { action: "new_game" }); 
+    socket.join(short_code); // joining game owner to game socket id (game.short_code)
     
     res.send(game);
   } catch (err: any) {
@@ -102,35 +98,24 @@ app.post('/new-game', async (req: any, res: any) => {
   }
 });
 
-// app.use('/api/taskRequest', taskRequest);
-
-
-
-// const joinGame = async () => {
-//   console.log('join game');
   
-  app.post('/join-game', async (req: any, res: any) => {
-    const short_code = req.query.code;
-    const { player } = req.body;
-  
-    try {
-      await addPlayerToGameMain(short_code, player);
-      const game = await getGameAndPlayers(short_code);
-      // TODO: Subscribe Player 2 to Game socket
-      // Need to send to client, trigger API call after they join??
-      const socket = req.app.get("socket");
-      socket.join(short_code);
-      
-      emitEvent("connected", game.short_code, { action: "new_player" });
-      res.send(game);
-    } catch (err: any) {
-      res.status(err.code ? err.code : 400).send(err.toString());
-    }
-  });
+app.post('/join-game', async (req: any, res: any) => {
+  const short_code = req.query.code;
+  const { player } = req.body;
 
-// }
+  try {
+    const socket = req.app.get("socket");
+    await addPlayerToGameMain(short_code, player);
+    const game = await getGameAndPlayers(short_code);
+    // join game room
+    socket.join(short_code);
+    io.to(short_code).emit("connected", { action: "new_player" });
 
-// app.use('/join-game', joinGame)
+    res.send(game);
+  } catch (err: any) {
+    res.status(err.code ? err.code : 400).send(err.toString());
+  }
+});
 
 app.get('/game/:short_code/players', async (req: any, res: any) => {
   // console.log(req.params.id);
@@ -145,15 +130,16 @@ app.get('/game/:short_code/players', async (req: any, res: any) => {
 });
 
 app.put('/player-ready/:playerID/:short_code', async (req: any, res: any) => {
-  console.log(req.params);
+  // console.log(req.params);
 
   const { playerID, short_code } = req.params;
   try {
     const result = await updatePlayerReady(playerID);
+    // TODO: Emit update players
     const allReady = await checkIfAllPlayersAreReady(short_code);
     if (allReady) {
       console.log('all players are ready');
-      io.to(short_code).emit("all_players_ready");
+      io.to(short_code).emit("connected", { action: "all_players_ready" });
     }
     res.send(result);
   } catch (err: any) {
